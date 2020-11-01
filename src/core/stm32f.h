5019ca8a846092d03e88f8902d14b8da5a45e87e
dma <daniel.m.aldea@hotmail.com>
2020-11-01 18:07:01 +0100

    feat: added first version of the startup and scatter files

---
+++ src/core/stm32f.h
@@ -0,0 +1,7 @@
+#ifndef STM32F_H
+#define STM32F_H
+#include <core/types.h>
+
+#define __INITIAL_SP            0x20002000
+
+#endif // STM32F_H
---
+++ src/core/types.h
@@ -0,0 +1,14 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef unsigned char BYTE;
+typedef unsigned short SHORT;
+typedef unsigned int WORD;
+typedef unsigned long int DWORD;
+
+typedef signed char SBYTE;
+typedef signed short SSHORT;
+typedef signed int SWORD;
+typedef signed long int SDWORD;
+
+#endif // TYPES_H
